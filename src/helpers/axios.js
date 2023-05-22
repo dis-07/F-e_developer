fce71cd692ba9193c40788a6c1980c728b46623d
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2023-05-22 16:58:36 +0300

    fixed api

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
-  // baseURL: 'https://f-e-developer.herokuapp.com/',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://f-e-developer.herokuapp.com/',
 });
 
 instance.interceptors.response.use((response) => response.data);
