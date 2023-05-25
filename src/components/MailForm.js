a263f17d61a2f2d7269e5050d17420507659a588
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2023-05-25 10:48:30 +0300

    added proxy

--- src/components/MailForm.js
+++ src/components/MailForm.js
@@ -12,7 +12,7 @@ const MailForm = ({ handleCloseForm }) => {
 
   const onSubmit = async (data) => {
     try {
-      await axios.post('/contact', data);
+      await axios.post('/feedback', data);
       // Обработка успешной отправки формы
       console.log('Форма успешно отправлена');
       handleCloseForm();
