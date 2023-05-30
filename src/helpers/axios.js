d655814d70abef76591811ace62824efd5b9363d
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2023-05-30 11:00:28 +0300

    added vercel proxi

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:3000',
-  baseURL: 'https://f-e-developer.herokuapp.com/',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://f-e-developer.herokuapp.com/api',
 });
 
 instance.interceptors.response.use((response) => response.data);
